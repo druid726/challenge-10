c831d569599a4a5285cab4a57751441a28d1475e
druid726 <pdoneill26@gmail.com>
2014-03-31 22:47:29 -0400

    first attempt at solving

--- js/tasklist.js
+++ js/tasklist.js
@@ -1,5 +1,44 @@
 $(document).ready(function(){
 
 	//write your code here.
-	
+
+	var taskName = '',//name of the task
+		taskNum = '',//number of the task
+		taskList = [];//list of tasks
+
+	// DISPLAY LIST //
+
+	function displayList() {
+
+
+	taskListArea.html ('') //clears the list
+
+	$.each(taskList, function(i, value) { //reiterating over the whole list
+		console.log(i+' '+value);
+	});
+
+	console.log
+}
+
+
+
+
+/*
+	$('#task_list').find('li').each(function(i){
+		console.log($(this).data('task_list'));
+		console.log($(this).data('task_name'));
+	});
+
+
+		/*var taskList = $(this).text();
+		taskList.push(task);
+	}
+
+	var taskName = $('input[name="task_name"]').val();
+
+
+	$('form').submit(function(event) {
+		event.preventDefault();
+	});
+	*/
 });
